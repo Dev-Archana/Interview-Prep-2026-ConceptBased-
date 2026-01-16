c163501560f2a6a3700b13d84370ccd543699285
Dlithe-JCER <incubationjcer@gmail.com>
2026-01-16 12:46:43 +0530

    Prep-start-1

---
+++ DataTypes/Sum_and_Differenceof_Two_Numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum,diff;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&num1,&num2);
+    sum = num1 + num2;
+    diff = num1 - num2;
+    printf("Sum: %d\n",sum);
+    printf("Difference: %.1d\n",diff);
+    return 0;
+}
